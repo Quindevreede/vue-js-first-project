aada20f97061322c6e7ad7b8d9c0ff690b68f378
Quindevreede <quindevreede@hotmail.com>
2022-07-24 01:55:37 +0200

    second commit tutorial chapter 1-4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div id="tutorial-one" v-cloak>
+      {{greeting}}
+      <input v-model="greeting" />
+      <hr />
+      <div class="tutorial-box1" v-if="isVisible1"></div>
+      <div class="tutorial-box2" v-else-if="isVisible2"></div>
+      <div class="tutorial-box3" v-else></div>
+      <hr>
+    </div>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      This is my first project
+      <a href="https://github.com/Quindevreede?tab=repositories" target="_blank" rel="noopener">GitHub Link</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Some Links</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
 </template>
@@ -33,6 +25,13 @@
 <script>
 export default {
   name: 'HelloWorld',
+  data: function () {
+    return {
+      greeting: "Type Something",
+      isVisible1: false,
+      isVisible2: false,
+    };
+  },
   props: {
     msg: String
   }
@@ -54,5 +53,23 @@ li {
 }
 a {
   color: #42b983;
+}
+.tutorial-box1 {
+  background-color: mediumseagreen;
+  height: 100px;
+  width: 100px;
+}
+.tutorial-box2 {
+  background-color: greenyellow;
+  height: 100px;
+  width: 100px;
+}
+.tutorial-box3 {
+  background-color: darkslategray;
+  height: 100px;
+  width: 100px;
+}
+[v-cloak] {
+  display: none;
 }
 </style>
